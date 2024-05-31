c7ca65a0eae14f6f3b00af42d00a57a849287f2d
jsdeveloper <js.developer@ongraph.com>
2024-05-31 12:33:41 +0530

    added base api url

--- packages/axios/index.ts
+++ packages/axios/index.ts
@@ -1,7 +1,7 @@
 import _axios from "axios";
 
 export const axios = _axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: process.env.BASE_API_URL || "http://localhost:5000/api",
   withCredentials: true,
   headers: {
     Accept: "application/json",
