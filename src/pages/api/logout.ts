5fa012bebde0ed423be4f6dffe053bb2dcb01ea3
jsdeveloper <js.developer@ongraph.com>
2024-06-20 17:57:16 +0530

    removing all cookies

--- src/pages/api/logout.ts
+++ src/pages/api/logout.ts
@@ -2,14 +2,17 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
-  const cookieKeys = ['session', 'session.sig'];
+ const cookies = req.headers.cookie;
+  
+ if (cookies) {
+   const cookieArray = cookies.split('; ');
 
-  const cookieStrings = cookieKeys.map(key => {
-    return `${key}=; Path=/; HttpOnly; Secure; SameSite=None; Max-Age=0; Expires=${new Date(0).toUTCString()}`;
-  });
-
-
-  res.setHeader('Set-Cookie', cookieStrings);
+   const cookieStrings = cookieArray.map(cookie => {
+     const [key] = cookie.split('=');
+     return `${key}=; Path=/; HttpOnly; Secure; SameSite=None; Max-Age=0; Expires=${new Date(0).toUTCString()}`;
+   });
+   res.setHeader('Set-Cookie', cookieStrings);
+ }
 
 
   res.status(200).json({ message: "Hello from the API!" });
