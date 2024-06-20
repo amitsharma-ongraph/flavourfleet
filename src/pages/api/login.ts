02ba1ab99730a5ebd1597e6218ff18084593dadc
jsdeveloper <js.developer@ongraph.com>
2024-06-20 17:24:49 +0530

    explicitly setting cookies

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -6,5 +6,13 @@ import { NextApiRequest, NextApiResponse } from "next";
 export default async  function handler(req: NextApiRequest, res: NextApiResponse) {
   const cookies=req.body.cookies
   console.log("server loing req cookies",cookies)
+  if (cookies) {
+    const cookieStrings = Object.entries(cookies).map(([key, value]) => {
+    
+      return `${key}heyhey=${value}; Path=/; HttpOnly; Secure; SameSite=None; Max-Age=3600`;
+    })
+    
+    res.setHeader('Set-Cookie', cookieStrings);
+  }
   res.status(200).json({ message: "Hello from the API!" });
 }
