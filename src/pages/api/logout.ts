5dabdde419c2f53cc2cf950bff3f7fc1d89aa0b5
jsdeveloper <js.developer@ongraph.com>
2024-06-21 11:32:20 +0530

    removing cookies from both

--- src/pages/api/logout.ts
+++ src/pages/api/logout.ts
@@ -9,7 +9,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
    console.log("logout cookie array",cookies);
    const cookieStrings = cookieArray.map(cookie => {
      const [key] = cookie.split('=');
-     return `${key}="null"; Path=/; HttpOnly; Secure; SameSite=None; Max-Age=100000; Expires=${new Date(0).toUTCString()}`;
+     return `${key}="null"; Path=/; HttpOnly; Secure; SameSite=None; Max-Age=0; Expires=${new Date(0).toUTCString()}`;
    });
    res.setHeader('Set-Cookie', cookieStrings);
  }
